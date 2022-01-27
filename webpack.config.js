592161b841884e9c1e48f405ff4cdc918213407d
Jannik Indorf <jannik.indorf@allgeier-it.de>
2022-01-27 10:14:36 +0100

    login form with store and localstorage - changed webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,52 @@
 const path = require("path");
 const webpack = require("webpack");
-const { VueLoaderPlugin } = require('vue-loader')
+const { VueLoaderPlugin } = require("vue-loader");
 
-module.exports = () => ({
-  name:"single-spa-auth-app",
-  entry:"./src/main.js",
-  mode:'development',
+module.exports = {
+  name: "single-spa-auth-app",
+  entry: "./src/main.js",
+  mode: "development",
   output: {
     library: "single-spa-auth-app",
     libraryTarget: "umd",
     filename: "single-spa-auth-app.js",
     path: path.resolve(__dirname, "dist"),
-    globalObject: 'this'
+    globalObject: "this",
   },
   module: {
     rules: [
       {
-        parser: {
-          system: false,
+        test: /\.(js)$/,
+        exclude: path.resolve(__dirname + "/node_modules/"),
+        use: {
+          loader: "babel-loader",
         },
       },
+
       {
-        test: /\.js?$/,
-        exclude: [path.resolve(__dirname, 'node_modules')],
-        loader: ['babel-loader', 'eslint-loader'],
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.module\.s(a|c)ss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[local]",
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
-      }
+        loader: "vue-loader",
+      },
     ],
   },
   plugins: [
@@ -42,4 +56,4 @@ module.exports = () => ({
     }),
   ],
   externals: ["bootstrap", "bootstrap-vue", "single-spa-vue", "vue", "vue-router", "vuex"],
-});
+};
